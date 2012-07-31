a862d1f0934cd76ee54d55771e50be9fe72f072b
Tim Gilyazetdinov <timg@timg-mac.local>
2012-07-31 12:58:21 -0400

    Initial commit

---
+++ exactHeightColumn.js
@@ -0,0 +1,13 @@
+// Adjusts the amount of children in a column according to the height of the column (removes childrent that overflow and partially hidden)
+$.fn.RGEColumn = function() {
+	return this.each(function(){
+		var column_height = $(this).css({'overflow':'hidden'}).height();
+
+		$(this).children().each(function(){
+			if( ($(this).position().top + $(this).outerHeight() - parseInt($(this).css('margin-bottom')) ) > column_height ){
+				$(this).hide();
+			}
+		});
+
+	});
+};
